2c4a9180c0eeff438dfa91c6a9b6d3d66d286728
jonah404 <jonahlalvarez@gmail.com>
2022-07-22 20:33:36 -0300

    creación de navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+export default function NavBar(){
+    return(
+        <nav>
+        <label for="" class="logo">
+            <a href="">Logo</a>
+        </label>
+
+        <ul>
+            <li><a href="">Productos</a></li>
+            <li><a href="">Nosotros</a></li>
+            <li><a href="">Contacto</a></li>
+        </ul>
+    </nav>
+    )
+}
